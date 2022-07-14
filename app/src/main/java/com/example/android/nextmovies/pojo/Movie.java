e3ca945df636e6650cba3ec99719b76454573fbc
AmarantBCL <vadbcl@outlook.com>
2022-07-14 17:28:29 +0300

    Data

---
+++ app/src/main/java/com/example/android/nextmovies/pojo/Movie.java
@@ -0,0 +1,65 @@
+package com.example.android.nextmovies.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+    @SerializedName("id")
+    private int id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("original_title")
+    private String originalTitle;
+    @SerializedName("overview")
+    private String overview;
+    @SerializedName("poster_path")
+    private String posterPath;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("vote_average")
+    private double averageVote;
+    @SerializedName("vote_count")
+    private int votes;
+
+    public Movie(int id, String title, String originalTitle, String overview, String posterPath, String releaseDate, double averageVote, int votes) {
+        this.id = id;
+        this.title = title;
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.releaseDate = releaseDate;
+        this.averageVote = averageVote;
+        this.votes = votes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public double getAverageVote() {
+        return averageVote;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+}
---
+++ app/src/main/java/com/example/android/nextmovies/pojo/MovieResponse.java
@@ -0,0 +1,18 @@
+package com.example.android.nextmovies.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse {
+    @SerializedName("results")
+    private List<Movie> movies;
+
+    public MovieResponse(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+}
